f168f52d47f10733580b0928230257c658cdd7a1
shenfeng1945 <823902826@qq.com>
2020-11-23 19:23:53 +0800

    搭建html页面

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from 'vue-router'
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
-// const router = createRouter();
-createApp(App).mount('#app')
+const router = createRouter({
+    history,
+    routes: [
+        {path: '/', component: Home},
+        {path: '/doc', component: Doc},
+    ]
+});
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <div class="topnav">
+         <div class="logo"></div>
+         <div class="menu"></div>
+       </div>
+       <div class="banner">
+         <h1>FlyVue</h1>
+         <h2>一个厉害的FlyVue</h2>
+         <div class="actions">
+           <a href="">Github</a>
+           <a href="">开始</a>
+         </div>
+       </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .topnav{
+        border: 1px solid red;
+    }
+</style>
