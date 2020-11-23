c66a56def42f12805563c87ea11dd17ba2487db7
shenfeng1945 <823902826@qq.com>
2020-11-23 20:56:09 +0800

    封装Topnav

---
+++ src/components/Topnav.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="topnav">
+    <div class="logo">Logo</div>
+    <div class="menu">
+      <ul>
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
+    </div>
+  </div>
+</template>
