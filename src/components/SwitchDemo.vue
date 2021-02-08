2837fb029d22464fa8ec148926eec7e20b865098
shenfeng1945 <823902826@qq.com>
2021-02-08 20:12:02 +0800

    新增 switch 组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-        Switch
+       <Switch v-model:value="value"/>
     </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
+import Switch from '../lib/Switch.vue'
 export default {
-    name: 'SwitchDemo'
+    name: 'SwitchDemo',
+    components: {
+        Switch,
+    },
+    setup(){
+        const value = ref(true);
+        return {value}
+    }
 }
 </script>
 
---
+++ src/lib/Switch.vue
@@ -0,0 +1,73 @@
+<template>
+  <label class="f-switch">
+    <span class="f-control-indicator" :class="{checked: value}" @click="toogle"></span>
+  </label>
+</template>
+
+<script>
+export default {
+    props: {
+        value: Boolean
+    },
+    setup(props, context){
+        const toogle = () => {
+           context.emit('update:value', !props.value)
+        }
+        return {toogle}
+    }
+};
+</script>
+
+<style lang="scss">
+.f-switch {
+  &-label {
+    display: inline-flex;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+    &.disabled {
+      cursor: not-allowed;
+    }
+  }
+  .f-control-indicator {
+    width: 30px;
+    background: rgba(167, 182, 194, 0.5);
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    min-width: 1.75em;
+    width: auto;
+    height: 1em;
+    border-radius: 1.75em;
+    transition: background-color 0.3s;
+    &.checked{
+        background: #137cbd;
+    }
+    &::before {
+      display: block;
+      position: absolute;
+      content: "";
+      width: calc(1em - 4px);
+      height: calc(1em - 4px);
+      border-radius: 50%;
+      background: white;
+      margin: 2px;
+      left: 0;
+      transition: left 0.3s;
+    }
+    &.checked::before {
+      left: calc(100% - 1em);
+    }
+    &.large {
+      height: 1.25em;
+      &::before {
+        width: 1em;
+        height: 1em;
+      }
+      &.checked::before {
+        left: calc(100% - 1.2em);
+      }
+    }
+  }
+}
+</style>
